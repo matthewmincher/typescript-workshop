b6edc100a25a09f0b737ec944dddc6af84bdb15b
Matthew Mincher <matthew.mincher@sykescottages.co.uk>
2023-10-02 01:03:12 +0100

    Leave records

--- console/src/getting-to-grips/resistor-calculator.ts
+++ console/src/getting-to-grips/resistor-calculator.ts
@@ -26,6 +26,33 @@ export const enum ResistorColour {
   Silver = "silver",
 }
 
+const digitMap: Partial<Record<ResistorColour, number>> = {
+  [ResistorColour.Black]: 0,
+  [ResistorColour.Brown]: 1,
+  [ResistorColour.Red]: 2,
+  [ResistorColour.Orange]: 3,
+  [ResistorColour.Yellow]: 4,
+  [ResistorColour.Green]: 5,
+  [ResistorColour.Blue]: 6,
+  [ResistorColour.Violet]: 7,
+  [ResistorColour.Grey]: 8,
+  [ResistorColour.White]: 9,
+};
+
+const multiplierMap: Partial<Record<ResistorColour, number>> = {
+  [ResistorColour.Black]: 1,
+  [ResistorColour.Brown]: 10,
+  [ResistorColour.Red]: 100,
+  [ResistorColour.Orange]: 1_000,
+  [ResistorColour.Yellow]: 10_000,
+  [ResistorColour.Green]: 100_000,
+  [ResistorColour.Blue]: 1_000_000,
+  [ResistorColour.Violet]: 10_000_000,
+  [ResistorColour.Grey]: 100_000_000,
+  [ResistorColour.Gold]: 0.1,
+  [ResistorColour.Silver]: 0.01,
+};
+
 export function calculateResistorValue(bands: ResistorInput): number {
-  return 0
+    return 0;
 }
