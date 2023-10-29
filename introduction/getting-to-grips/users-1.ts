e56e5638d06bc7cce0e6e210e6cb98c094427fa4
Matthew Mincher <matthew.mincher@sykescottages.co.uk>
2023-10-29 15:25:07 +0000

    Add g2g #1

---
+++ introduction/getting-to-grips/users-1.ts
@@ -0,0 +1,23 @@
+/**
+ * Define a type for a pet and replace the umknowns in this file
+ * npx ts-node getting-to-grips/users-1.ts
+ */
+
+export type Pet = unknown;
+
+export const pets: unknown[] = [
+  {
+    name: "Spud",
+    age: 8,
+    species: "Cat",
+  },
+  {
+    name: "Fred",
+    age: 27,
+    species: "Tortoise",
+  },
+];
+
+pets.forEach((pet, i) => {
+  console.log(`Pet at ${i}: ${pet.name} - ${pet.age} yr old ${pet.species}`);
+});
